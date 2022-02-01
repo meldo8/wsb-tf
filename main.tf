d4905e894c81fb58f7fdb70d63cdd565fd2358cf
Dominik Melnarowicz <dominik.melnarowicz@netguru.com>
2022-02-01 20:54:58 +0100

    init

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_iam_role" "lambda_exec" {
   name = "serverless_lambda"
 
   assume_role_policy = jsonencode({
-    Version = "2022-02-01"
+    Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
